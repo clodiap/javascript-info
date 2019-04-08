1136b003cab8b89459403f3e5041f89fa886bdb9
Clodiap <clodiapp@gmail.com>
2019-04-08 17:49:07 +0200

    fundamentals - suite

---
+++ fundamentals-operators.js
@@ -0,0 +1,11 @@
+// What are the final values of all variables a, b, c and d after the code below?
+
+let a = 1, b = 1;
+
+let c = ++a; // a = 2, c = 2
+let d = b++; // b = 2, d = 1
+
+//What are the values of a and x after the code below?
+
+let a = 2;
+let x = 1 + (a *= 2); // a = 4, x = 5
