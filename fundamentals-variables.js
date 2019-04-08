ad684dfe30b28891715a6032cb7a32ca86e1e9e8
Clodiap <clodiapp@gmail.com>
2019-04-08 11:17:51 +0200

    fundamentals

---
+++ fundamentals-variables.js
@@ -0,0 +1,18 @@
+// exercise 1
+let admin,
+  name;
+
+name = "John";
+admin = name;
+
+alert(admin)
+
+// exercise 2
+
+let ourPlanet = "earth";
+let currentUserName = "John";
+
+// exercise 3
+
+const BIRTHDAY = '18.04.1982';
+const age = someCode(BIRTHDAY);
